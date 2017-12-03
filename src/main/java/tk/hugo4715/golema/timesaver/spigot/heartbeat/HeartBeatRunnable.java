8c333be3f8e7597c89f62ffe440db4574d648172
BuildTools <unconfigured@null.spigotmc.org>
2017-12-03 20:34:04 +0100

    crash detect

--- src/main/java/tk/hugo4715/golema/timesaver/spigot/heartbeat/HeartBeatRunnable.java
+++ src/main/java/tk/hugo4715/golema/timesaver/spigot/heartbeat/HeartBeatRunnable.java
@@ -5,6 +5,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import redis.clients.jedis.Jedis;
 import tk.hugo4715.golema.timesaver.TimeSaverAPI;
+import tk.hugo4715.golema.timesaver.server.GameInfos;
 import tk.hugo4715.golema.timesaver.server.ServerInfo;
 import tk.hugo4715.golema.timesaver.server.ServerType;
 import tk.hugo4715.golema.timesaver.spigot.TSSpigot;
@@ -15,48 +16,53 @@ import tk.hugo4715.golema.timesaver.spigot.TSSpigot;
 public class HeartBeatRunnable extends BukkitRunnable {
 
 	private int inactifTime = 0;
-
+	private int crashTime = 0;
+	
 	public HeartBeatRunnable() {}
 
 	public void run() {
 
-		// Serveur.
-		if (TSSpigot.getCurrentServerInfos() != null) {
+		// Détection d'un crash.
+		if((TSSpigot.getCurrentServerInfos() == null)
+				|| (TSSpigot.getCurrentServerInfos().getGameInfos().getName().equalsIgnoreCase(GameInfos.NONE.getName()))) {
+			if(crashTime == 10) Bukkit.getServer().shutdown(); 
+			crashTime++;
+			return;
+		}
 
-			// Update du serveur sur Redis.
-			try (Jedis j = TSSpigot.get().getCommon().getJedisAccess().getJedisPool().getResource()) {
-				TSSpigot.getCurrentServerInfos().setCurrentPlayers(Bukkit.getOnlinePlayers().size());
-				TSSpigot.getCurrentServerInfos().setMaxPlayers(Bukkit.getMaxPlayers());
+		// Update du serveur sur Redis.
+		try (Jedis j = TSSpigot.get().getCommon().getJedisAccess().getJedisPool().getResource()) {
+			TSSpigot.getCurrentServerInfos().setCurrentPlayers(Bukkit.getOnlinePlayers().size());
+			TSSpigot.getCurrentServerInfos().setMaxPlayers(Bukkit.getMaxPlayers());
 
-				String key = "TS:SERVERS:" + TSSpigot.getCurrentServerInfos().getId();
-				j.set(key, TSSpigot.get().getCommon().getGson().toJson(TSSpigot.getCurrentServerInfos()));
-				j.expire(key, 30);
+			String key = "TS:SERVERS:" + TSSpigot.getCurrentServerInfos().getId();
+			j.set(key, TSSpigot.get().getCommon().getGson().toJson(TSSpigot.getCurrentServerInfos()));
+			j.expire(key, 30);
+		}
+
+		// Détection de serveur innactif.
+		if (Bukkit.getOnlinePlayers().size() > 0) {
+			inactifTime = 0;
+		}
+
+		// Serveur innactif trop longtemps. (timeout)
+		if (inactifTime >= 30) {
+			int serverTypeCount = 0;
+			for (ServerInfo sInfo : TimeSaverAPI.getServerInfoList()) {
+				if ((sInfo != null) && (sInfo.isJoinable())
+						&& (sInfo.getGameInfos().getName()
+								.equalsIgnoreCase(TSSpigot.getCurrentServerInfos().getGameInfos().getName()))
+						&& ((sInfo.getType().getName().equalsIgnoreCase(ServerType.LOBBY.getName()))
+								|| (sInfo.getType().getName().equalsIgnoreCase(ServerType.GAME.getName())))) {
+					serverTypeCount++;
+				}
 			}
-
-			// Détection de serveur innactif.
-			if (Bukkit.getOnlinePlayers().size() > 0) {
+			if (serverTypeCount >= 2) {
+				Bukkit.getServer().shutdown();
+			} else {
 				inactifTime = 0;
 			}
-
-			// Serveur innactif trop longtemps. (timeout)
-			if (inactifTime >= 30) {
-				int serverTypeCount = 0;
-				for (ServerInfo sInfo : TimeSaverAPI.getServerInfoList()) {
-					if ((sInfo != null) && (sInfo.isJoinable())
-							&& (sInfo.getGameInfos().getName()
-									.equalsIgnoreCase(TSSpigot.getCurrentServerInfos().getGameInfos().getName()))
-							&& ((sInfo.getType().getName().equalsIgnoreCase(ServerType.LOBBY.getName()))
-									|| (sInfo.getType().getName().equalsIgnoreCase(ServerType.GAME.getName())))) {
-						serverTypeCount++;
-					}
-				}
-				if (serverTypeCount >= 2) {
-					Bukkit.getServer().shutdown();
-				} else {
-					inactifTime = 0;
-				}
-			}
-			inactifTime++;
 		}
+		inactifTime++;
 	}
 }
